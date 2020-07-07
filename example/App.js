77b308cdc0c2ea0e8003189f32f28ce537992136
Love Ahnström <love@itsitchy.com>
2020-07-07 17:24:57 +0200

    Hard code item height

--- example/App.js
+++ example/App.js
@@ -1,93 +1,74 @@
-/**
- * Sample React Native Calendar Strip
- * https://github.com/BugiDev/react-native-calendar-strip
- * @flow
- */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import CS from "./CalendarStrip";
 
-import React, { Component } from 'react';
-import { View, Text } from "react-native";
-import CalendarStrip from './CalendarStrip/CalendarStrip';
-import moment from 'moment';
-
-export default class App extends Component<{}> {
-  constructor(props) {
-    super(props);
-
-    let startDate = moment(); // today
-
-    // Create a week's worth of custom date styles and marked dates.
-    let customDatesStyles = [];
-    let markedDates = [];
-    for (let i=0; i<7; i++) {
-      let date = startDate.clone().add(i, 'days');
-
-      customDatesStyles.push({
-        startDate: date, // Single date since no endDate provided
-        dateNameStyle: {color: 'blue'},
-        dateNumberStyle: {color: 'purple'},
-        // Random color...
-        dateContainerStyle: { backgroundColor: `#${(`#00000${(Math.random() * (1 << 24) | 0).toString(16)}`).slice(-6)}` },
-      });
-
-      let dots = [];
-      let lines = [];
-
-      if (i % 2) {
-        lines.push({
-          color: 'cyan',
-          selectedColor: 'orange',
-        });
-      }
-      else {
-        dots.push({
-          color: 'red',
-          selectedColor: 'yellow',
-        });
-      }
-      markedDates.push({
-        date,
-        dots,
-        lines
-      });
-    }
-
-    this.state = {
-      selectedDate: '',
-      customDatesStyles,
-      markedDates,
-      startDate,
-    };
-  }
-
-  datesBlacklistFunc = date => {
-    return date.isoWeekday() === 6; // disable Saturdays
-  }
-
-  onDateSelected = date => {
-    this.setState({ formattedDate: date.format('YYYY-MM-DD')});
-  }
-
-  render() {
-    return (
-      <View>
-        <CalendarStrip
-          scrollable
-          calendarAnimation={{type: 'sequence', duration: 30}}
-          daySelectionAnimation={{type: 'background', duration: 300, highlightColor: '#9265DC'}}
-          style={{height:200, paddingTop: 20, paddingBottom: 10}}
-          calendarHeaderStyle={{color: 'white'}}
-          calendarColor={'#3343CE'}
-          dateNumberStyle={{color: 'white'}}
-          dateNameStyle={{color: 'white'}}
-          iconContainer={{flex: 0.1}}
-          customDatesStyles={this.state.customDatesStyles}
-          markedDates={this.state.markedDates}
-          datesBlacklist={this.datesBlacklistFunc}
-          onDateSelected={this.onDateSelected}
-          useIsoWeekday={false}
-        />
-        <Text style={{fontSize: 24}}>Selected Date: {this.state.formattedDate}</Text>
+// eslint-disable-next-line react/display-name
+const Date = React.memo(({ date, marking, ...rest }) => {
+  console.log(JSON.stringify(rest, null, 2));
+  return (
+    <TouchableOpacity style={styles.date}>
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateText}>{date?.format("ddd")}</Text>
+        <Text style={styles.dateNumber}>{date?.format("D")}</Text>
+        <View style={styles.dotContainer}>
+          {/* {marking?.dots?.map?.(({ color: c }) => (
+            // eslint-disable-next-line react/jsx-key
+          ))} */}
+          <View style={[styles.dot, { backgroundColor: "blue" }]} />
+        </View>
       </View>
-    );
-  }
+    </TouchableOpacity>
+  );
+});
+
+export default function CalendarStrip({ ...rest }) {
+  return (
+    <View style={{ paddingTop: 200 }}>
+      <CS
+        // calendarHeaderContainerStyle={styles.headerContainer}
+        iconLeft={null}
+        iconRight={null}
+        style={{ width: "100%", height: 120 }}
+        dayComponent={Date}
+        maxDayComponentSize={200}
+        scrollable
+        anotherProp
+        {...rest}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: { fontSize: 18 },
+  dateName: {
+    fontSize: 12,
+    textTransform: "capitalize",
+  },
+  markedDates: {
+    color: "blue",
+  },
+  dateContainer: {
+    alignItems: "center",
+    overflow: "visible",
+  },
+  date: { overflow: "visible" },
+  dot: {
+    width: 6,
+    height: 6,
+    borderRadius: 6,
+    marginHorizontal: 2,
+  },
+  dateText: {
+    marginBottom: 14,
+    fontSize: 12,
+  },
+  dateNumber: {
+    fontSize: 12,
+  },
+  dotContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+});
